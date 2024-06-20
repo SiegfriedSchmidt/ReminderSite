6a5fb054d37bdffdf36b28f8ee23316a4a51a230
Matvei <matveiyan@yandex.ru>
2024-06-20 15:16:29 +0300

    More jwt

--- backend/lib/config_reader.py
+++ backend/lib/config_reader.py
@@ -1,12 +1,13 @@
-from pydantic import SecretStr
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr, BaseSettings
 from pathlib import Path
 
 env_path = Path(__file__).parent.parent.absolute() / Path('.env')
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=env_path, env_file_encoding='utf-8')
-    jwt_secret_key: SecretStr = 'xxx'
+    jwt_secret_key: SecretStr
 
+    class Config:
+        env_file = env_path
+        env_file_encoding = 'utf-8'
 
 config = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
-from typing import Union
-from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi import HTTPException, Depends, Request, FastAPI, APIRouter
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
@@ -9,8 +8,9 @@ import asyncio
 import uvicorn
 
 create_tables()
-app = FastAPI(root_path='/api')
 
+router = APIRouter(prefix='/api')
+app = FastAPI()
 
 @AuthJWT.load_config
 def get_config():
@@ -24,29 +24,40 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
         content={"detail": exc.message}
     )
 
+@router.get('/')
+async def main(request: Request):
+    return {'title': 'about'}
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@router.post('/refresh')
+async def refresh(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_refresh_token_required()
+
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    return {"access_token": new_access_token}
 
 
-@app.get("/events/{username}")
-async def read_item(username: str, q: Union[str, None] = None):
-    print(username, q)
-    print(Event.select())
-    return 'lol'
+@router.post('/login')
+async def login(user: UserPydantic, Authorize: AuthJWT = Depends()):
+    if not User.select().where(User.username == user.username).exists():
+        raise HTTPException(status_code=401, detail="Bad username or password")
+
+    access_token = Authorize.create_access_token(subject=user.username)
+    refresh_token = Authorize.create_refresh_token(subject=user.username)
+    return {"access_token": access_token, "refresh_token": refresh_token}
 
 
-@app.post('/login')
-async def login(user: User, Authorize: AuthJWT = Depends()):
+@router.post('/register')
+async def login(user: UserPydantic, Authorize: AuthJWT = Depends()):
     if user.username != "test" or user.password != "test":
         raise HTTPException(status_code=401, detail="Bad username or password")
 
     access_token = Authorize.create_access_token(subject=user.username)
-    return {"access_token": access_token}
+    refresh_token = Authorize.create_refresh_token(subject=user.username)
+    return {"access_token": access_token, "refresh_token": refresh_token}
 
 
-@app.get('/user')
+@router.get('/user')
 async def user(Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
 
@@ -55,6 +66,7 @@ async def user(Authorize: AuthJWT = Depends()):
 
 
 async def main():
+    app.include_router(router)
     config = uvicorn.Config(app, host='0.0.0.0', port=8000, log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
