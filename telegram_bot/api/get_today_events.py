78332d6d9a29c04d10c537a928f115c5a2fb0b2d
Matvei <matveiyan@yandex.ru>
2024-06-26 22:06:54 +0300

    change to localhost

--- telegram_bot/api/get_today_events.py
+++ telegram_bot/api/get_today_events.py
@@ -3,7 +3,7 @@ import requests
 from config_reader import config
 
 headers = {'token': config.internal_token.get_secret_value()}
-rs = requests.get('http://0.0.0.0:8003/api/internal/get_today_events', headers=headers)
+rs = requests.get('http://localhost:8003/api/internal/get_today_events', headers=headers)
 
 data = rs.json()
 pprint(data['events'])
