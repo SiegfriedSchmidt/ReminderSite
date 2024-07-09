302d72d7c4fb4fc536650c8a3cf004a219fe7d7e
Matvei <matveiyan@yandex.ru>
2024-07-09 17:48:09 +0300

    Add more docker composes

---
+++ MainNginx/generate-default-ssl.sh
@@ -0,0 +1,1 @@
+openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/default-ssl.key -out /etc/ssl/default-ssl.crt -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
